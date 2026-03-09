8a4c01b70e1f318d5da6693ff8ea38ee68a52f47
Mike Sperber <sperber@deinprogramm.de>
2026-03-09 10:42:58 +0100

    Update.

--- java/src/main/java/de/activegroup/Animal.java
+++ java/src/main/java/de/activegroup/Animal.java
@@ -1,6 +1,11 @@
 package de.activegroup;
 
 public sealed interface Animal {
+
+    default public Animal runOver() {
+        switch (this)
+    }
+
     /*
      * Gürteltier hat folgende Eigenschaften:
      * - lebendig oder tot        -UND-
@@ -8,8 +13,21 @@ public sealed interface Animal {
      * zusammengesetzte Daten / Produkt
     */
     record Dillo(Liveness liveness, double weight) implements Animal {
+        // @Override
         public Dillo runOver() {
             return new Dillo(Liveness.DEAD, weight);
+        }
+    }
+
+    /*
+     * Eine Klapperschlange hat:
+     * - Länge  UND
+     * - Dicke
+     */
+    public record Snake(double length, double width) implements Animal {
+        // @Override
+        public Snake runOver() {
+            return new Snake(this.length, 0);
         }
     }
 
--- java/src/main/java/de/activegroup/Dillo.java
+++ java/src/main/java/de/activegroup/Dillo.java
@@ -1,9 +1,2 @@
 package de.activegroup;
 
-/*
- * Tier auf dem texanischen Highyway:
- * - Gürteltier      ODER
- * - Klapperschlange
- * Summe
- *
- * Datendefinition:
