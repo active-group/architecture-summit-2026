5a367500fcd09114acec5d3f55728ac786bf7d54
Mike Sperber <sperber@deinprogramm.de>
2026-03-09 11:10:11 +0100

    Update.

--- java/src/main/java/de/activegroup/Main.java
+++ java/src/main/java/de/activegroup/Main.java
@@ -7,5 +7,10 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         var product1 = new ShowerProduct.Soap(7);
+        var product2 = new ShowerProduct.Shampoo(HairType.OILY);
+        var product3 = new ShowerProduct.ShowerGel(product1, product2);
+        var product4 = new ShowerProduct.Soap(9);
+        var product5 = new ShowerProduct.ShowerGel(product3, product4);
+
     }
 }
