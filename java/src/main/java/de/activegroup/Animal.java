d6af0b84de9659351fc62bce80d259c229c31551
Mike Sperber <sperber@deinprogramm.de>
2026-03-09 10:37:56 +0100

    Update.

--- java/src/main/java/de/activegroup/Animal.java
+++ java/src/main/java/de/activegroup/Animal.java
@@ -1,4 +1,16 @@
 package de.activegroup;
 
 public sealed interface Animal {
+    /*
+     * Gürteltier hat folgende Eigenschaften:
+     * - lebendig oder tot        -UND-
+     * - Gewicht
+     * zusammengesetzte Daten / Produkt
+    */
+    record Dillo(Liveness liveness, double weight) {
+        public Dillo runOver() {
+            return new Dillo(Liveness.DEAD, weight);
+        }
+    }
+
 }
--- java/src/main/java/de/activegroup/Dillo.java
+++ java/src/main/java/de/activegroup/Dillo.java
@@ -7,14 +7,3 @@ package de.activegroup;
  * Summe
  *
  * Datendefinition:
- * Gürteltier hat folgende Eigenschaften:
- * - lebendig oder tot        -UND-
- * - Gewicht
- * zusammengesetzte Daten / Produkt
- */
-
-public record Dillo(Liveness liveness, double weight) {
-    public Dillo runOver() {
-        return new Dillo(Liveness.DEAD, weight);
-    }
-}
